f91eb265e9878a7bc805e9925f4dff490e226274
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-03-03 11:32:15 -0400

    correção sql

--- sql/sqlAviaras.sql
+++ sql/sqlAviaras.sql
@@ -5,28 +5,28 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema bancaviaras
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema bancaviaras
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `bancaviaras` DEFAULT CHARACTER SET utf8 ;
+USE `bancaviaras` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`carrinho`
+-- Table `bancaviaras`.`carrinho`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`carrinho` (
+CREATE TABLE IF NOT EXISTS `bancaviaras`.`carrinho` (
   `id` INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`usuarios`
+-- Table `bancaviaras`.`usuarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`usuarios` (
+CREATE TABLE IF NOT EXISTS `bancaviaras`.`usuarios` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
@@ -36,16 +36,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`usuarios` (
   INDEX `fk_usuarios_carrinho1_idx` (`carrinho_id` ASC) VISIBLE,
   CONSTRAINT `fk_usuarios_carrinho1`
     FOREIGN KEY (`carrinho_id`)
-    REFERENCES `mydb`.`carrinho` (`id`)
+    REFERENCES `bancaviaras`.`carrinho` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`livros`
+-- Table `bancaviaras`.`livros`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`livros` (
+CREATE TABLE IF NOT EXISTS `bancaviaras`.`livros` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `sinopse` VARCHAR(45) NOT NULL,
@@ -56,16 +56,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`livros` (
   INDEX `fk_livros_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
   CONSTRAINT `fk_livros_usuarios1`
     FOREIGN KEY (`usuarios_id`)
-    REFERENCES `mydb`.`usuarios` (`id`)
+    REFERENCES `bancaviaras`.`usuarios` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`imagens`
+-- Table `bancaviaras`.`imagens`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`imagens` (
+CREATE TABLE IF NOT EXISTS `bancaviaras`.`imagens` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `livros_id` INT NOT NULL,
@@ -73,16 +73,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`imagens` (
   INDEX `fk_imagens_livros_idx` (`livros_id` ASC) VISIBLE,
   CONSTRAINT `fk_imagens_livros`
     FOREIGN KEY (`livros_id`)
-    REFERENCES `mydb`.`livros` (`id`)
+    REFERENCES `bancaviaras`.`livros` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`comentarios`
+-- Table `bancaviaras`.`comentarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`comentarios` (
+CREATE TABLE IF NOT EXISTS `bancaviaras`.`comentarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `texto` VARCHAR(45) NOT NULL,
@@ -93,21 +93,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`comentarios` (
   INDEX `fk_comentarios_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
   CONSTRAINT `fk_comentarios_livros1`
     FOREIGN KEY (`livros_id`)
-    REFERENCES `mydb`.`livros` (`id`)
+    REFERENCES `bancaviaras`.`livros` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_comentarios_usuarios1`
     FOREIGN KEY (`usuarios_id`)
-    REFERENCES `mydb`.`usuarios` (`id`)
+    REFERENCES `bancaviaras`.`usuarios` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`livros_has_carrinho`
+-- Table `bancaviaras`.`livros_has_carrinho`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`livros_has_carrinho` (
+CREATE TABLE IF NOT EXISTS `bancaviaras`.`livros_has_carrinho` (
   `livros_id` INT NOT NULL,
   `carrinho_id` INT NOT NULL,
   PRIMARY KEY (`livros_id`, `carrinho_id`),
@@ -115,12 +115,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`livros_has_carrinho` (
   INDEX `fk_livros_has_carrinho_livros1_idx` (`livros_id` ASC) VISIBLE,
   CONSTRAINT `fk_livros_has_carrinho_livros1`
     FOREIGN KEY (`livros_id`)
-    REFERENCES `mydb`.`livros` (`id`)
+    REFERENCES `bancaviaras`.`livros` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_livros_has_carrinho_carrinho1`
     FOREIGN KEY (`carrinho_id`)
-    REFERENCES `mydb`.`carrinho` (`id`)
+    REFERENCES `bancaviaras`.`carrinho` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
