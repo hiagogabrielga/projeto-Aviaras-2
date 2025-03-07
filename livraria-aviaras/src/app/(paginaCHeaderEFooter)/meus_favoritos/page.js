9b5709e56f25f37c478bc4cc48d5914eb8fe6dc5
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-03-07 13:01:20 -0400

    subindo

--- livraria-aviaras/src/app/(paginaCHeaderEFooter)/meus_favoritos/page.js
+++ livraria-aviaras/src/app/(paginaCHeaderEFooter)/meus_favoritos/page.js
@@ -82,38 +82,41 @@ const imagensHome = [
 
 export default function Home() {
     return (
-        <div className={styles.mae}>
-            <h2 className={styles.titulo}>Meus favoritos</h2>
-            <div className={styles.segundaParte}>
-                {
-                    imagensHome.map((item, index) => (
-                        <div key={index} className={styles.divItem}>
-                            <div className={styles.campoImagem}>
-                                <ul className={styles.menu}>
-                                    <li><Ellipsis size={40} />
-                                        <ul className={styles.opcoes}>
-                                            <li><button className={styles.excluir} onClick={() => {alert('Item excluido dos favoritos, com sucesso!')}}>Desfavoritar</button></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <Image
-                                    className={styles.imgCard}
-                                    src={item.imagem} // Adicione o caminho da imagem
-                                    alt={`Imagem_${index + 1}`} // Adicione um texto alternativo
-                                    width={190}
-                                    height={300}
-                                />
-                            </div>
-                            <div className={styles.campoTexto}>
-                                <h3>{item.titulo} {index + 1}</h3>
-                                <p className={styles.desc}>{item.descricao}</p>
-                                <p className={styles.mais}> <Link href="/ver_livro"> Ver mais</Link></p>
-                            </div>
+        <div className="containerParaHeaderELateralbar">
+            <div className={styles.mae}>
+                <h2 className={styles.titulo}>Meus favoritos</h2>
+                <div className={styles.segundaParte}>
+                    {
+                        imagensHome.map((item, index) => (
+                            <div key={index} className={styles.divItem}>
+                                <div className={styles.campoImagem}>
+                                    <ul className={styles.menu}>
+                                        <li><Ellipsis size={40} />
+                                            <ul className={styles.opcoes}>
+                                                <li><button className={styles.excluir} onClick={() => { alert('Item excluido dos favoritos, com sucesso!') }}>Desfavoritar</button></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <Image
+                                        className={styles.imgCard}
+                                        src={item.imagem} // Adicione o caminho da imagem
+                                        alt={`Imagem_${index + 1}`} // Adicione um texto alternativo
+                                        width={190}
+                                        height={300}
+                                    />
+                                </div>
+                                <div className={styles.campoTexto}>
+                                    <h3>{item.titulo} {index + 1}</h3>
+                                    <p className={styles.desc}>{item.descricao}</p>
+                                    <p className={styles.mais}> <Link href="/ver_livro"> Ver mais</Link></p>
+                                </div>
 
-                        </div>
-                    ))
-                }
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
+
     );
 }
